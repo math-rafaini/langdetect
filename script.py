9a88eed0e4645e8272d197261f5028b6c5142a7c
Matheus Fernandes Rafaini <matheus.frafaini168@gmail.com>
2023-06-09 20:50:15 -0300

    Add script.py

--- script.py
+++ script.py
@@ -2,3 +2,28 @@ import pandas as pd
 from langdetect import detect
 
 # Language detection
+
+df = pd.read_excel('texts.xlsx')
+
+langs = []
+words = []
+
+for index, row in df.iterrows():
+    lang = detect(row['Words'])
+    if(lang == "dt"):
+        lang = "German"
+
+    elif(lang == "en"):
+        lang = "English"
+
+    elif(lang == "pt"):
+        lang = "Portuguese"
+
+    else:
+        lang = lang + " - ISO 639-1"
+
+    langs.append(lang)
+    words.append(row['Words'])
+
+df = pd.DataFrame({'Words': words, 'Languages detected': langs})
+print(df)
